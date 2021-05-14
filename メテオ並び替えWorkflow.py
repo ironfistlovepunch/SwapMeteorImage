5df648945144c84c2b0f59a71ccddc2e385f21dc
ironfistlovepunch <79779590+ironfistlovepunch@users.noreply.github.com>
2021-05-15 00:07:48 +0900

    Add files via upload

--- メテオ並び替えWorkflow.py
+++ メテオ並び替えWorkflow.py
@@ -1,4 +1,6 @@
+import os
 import sys
+import glob
 import importlib
 import unicodedata
 import webbrowser
@@ -13,9 +15,39 @@ except ModuleNotFoundError as e:
 		name_nfd = unicodedata.normalize("NFD",name)
 		メテオ並び替え = importlib.import_module(name_nfd)
 
-print(sys.argv[1],sys.argv[2])
+#ファイルの所在を返す
+def get_file_path(path,extension):
+	file_name = os.path.basename(path)
+	split_name = os.path.splitext(file_name)[0]
+	file_dir = os.path.dirname(path)
+	
+	file_path = os.path.join(file_dir,split_name)
+	l = glob.glob(file_path + "*." + extension)
+	return l[0]
+
+#入力ファイル関係
+print(len(sys.argv))
+
+if len(sys.argv) == 3:
+	image = sys.argv[1]
+	json = sys.argv[2]
+else:
+	#テスト用
+	image = "0003.jpg"
+	json = "0003.ptimg.json"
+	work_path = "/private/var/mobile/Library/Mobile Documents/iCloud~is~workflow~my~workflows/Documents/作業用/メテオ画像合成/20210514195727/"
+	image = work_path + image
+	json = work_path + json
+
+#ファイル所在関連
+image_path = get_file_path(image,"jpg")
+print(image_path)
+json_path = get_file_path(json,"json")
+print(json_path)
+
 #呼ぶ
-メテオ並び替え.main(sys.argv[1],sys.argv[2])
+メテオ並び替え.main(image_path,json_path)
 
 #戻る
 webbrowser.open("workflow://")
+
